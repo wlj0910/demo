0b4b241217fe645762a7e27f565c214c644a0075
wlj0910 <1002617939@qq.com>
2019-07-09 17:45:56 +0800

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -108,6 +108,7 @@ public class Main {
     }
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("基础知识测试：");
         //demoString();
         //demoMapTable();
         //demoSet();
